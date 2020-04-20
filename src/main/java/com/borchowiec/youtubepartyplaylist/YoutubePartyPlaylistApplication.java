c1ba3bff6585fe865aa2977130ab5cfd97bfae30
borchowiec <borchowiecpatryk@gmail.com>
2020-04-20 09:20:56 +0200

    Init

---
+++ src/main/java/com/borchowiec/youtubepartyplaylist/YoutubePartyPlaylistApplication.java
@@ -0,0 +1,13 @@
+package com.borchowiec.youtubepartyplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubePartyPlaylistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubePartyPlaylistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/borchowiec/youtubepartyplaylist/YoutubePartyPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borchowiec.youtubepartyplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubePartyPlaylistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
