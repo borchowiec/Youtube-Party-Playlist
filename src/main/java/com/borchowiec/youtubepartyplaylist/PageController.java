6bf2b641b615c35ab17eabf429f768db0110f0a6
borchowiec <borchowiecpatryk@gmail.com>
2020-04-21 17:17:06 +0200

    Refactor

--- src/main/java/com/borchowiec/youtubepartyplaylist/PageController.java
+++ src/main/java/com/borchowiec/youtubepartyplaylist/PageController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+/**
+ * Returns pages of website.
+ */
 @Controller
 public class PageController {
     /**
