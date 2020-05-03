b60951f770806dd3bd484dc197c5173b714048ba
borchowiec <borchowiecpatryk@gmail.com>
2020-05-03 13:34:46 +0200

    Created layout. Added button that copies invitation link

---
+++ src/main/resources/static/js/authorizationPanel.js
@@ -0,0 +1,37 @@
+const authorizationPanelHtml = `
+<h2 class="title has-text-centered">Invite</h2>
+<div class="columns is-mobile">
+    <div class="column is-narrow"><input type="checkbox" id="accessByLinkCheckbox"></div>
+    <div class="column"><p class="accessPropertyLabel">Access only with link</p></div><br>
+</div>
+<div>
+    <div class="columns" style="padding: 0 40px">
+        <button class="button is-danger is-light is-fullwidth" id="copyInviteLink">Copy invite link</button>
+    </div>
+    <div class="columns" style="margin-top: 10px; padding: 0 40px">
+        <button class="button is-danger is-light is-fullwidth">Show invite QR code</button>
+    </div>
+</div>
+`;
+
+function copyInviteLink() {
+    const userToken = Cookies.get("userToken");
+    if (userToken) {
+        const $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val(`${window.location.origin}/auth/${userToken}`).select();
+        document.execCommand("copy");
+        $temp.remove();
+
+        const copyBtn = $("#copyInviteLink");
+        copyBtn.text("Copied!");
+        setTimeout(() => copyBtn.text("Copy invite link"), 1000);
+    }
+}
+
+$(document).ready(function() {
+    const base = $("#authorizationPanel");
+    base.append(authorizationPanelHtml);
+
+    $("#copyInviteLink").on("click", () => copyInviteLink());
+});
